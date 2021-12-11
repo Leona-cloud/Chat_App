cc60ca7aa55035125bed4961fb673efa62138bbc
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-12-11 22:46:33 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,15 @@
 'use strict'
 
-module.exports = function(){
+module.exports = function( _ ){
     
+    return {
+        setRoute: function(router){
+            router.get('/', this.startPage)
+        },
+
+        startPage: function(req, res){
+            return res.render('start', {start: "This is the start page"})
+        }
+    }
+
 }
