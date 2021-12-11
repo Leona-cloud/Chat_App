ed2742585b5feb7158e05c127041373530e3cec8
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-12-11 14:36:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+
+container.resolve((users)=>{
+
+    const app = setupExpress();
+
+    function setupExpress(){
+
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, ()=>{
+            console.log('listening on port 3000')
+        });
+
+        configureExpress(app);
+
+            //setup router
+        const router = require('express-promise-router')();
+        users.setRoute(router);
+
+        app.use(router);
+    }
+
+
+
+    function configureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(express.json());
+        app.use(express.urlencoded({extended: true}))
+    }
+
+});
