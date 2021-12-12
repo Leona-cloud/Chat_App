badce5a2d3e8287cfdb57d4392ed4d6658440f82
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-12-12 16:38:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,63 @@
-const express = require('express');
-const ejs = require('ejs');
-const http = require('http');
-const container = require('./container');
+const express = require("express");
+const ejs = require("ejs");
+const http = require("http");
+const container = require("./container");
+const cookieParser = require("cookie-parser");
+
+const session = require("express-session");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo");
+const passport = require('passport')
 
 
-container.resolve(function(users){
+container.resolve(function (users) {
+  mongoose.Promise = global.Promise;
+  mongoose.connect("mongodb://localhost/notes", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=> console.log(`connected to `))
+  .catch((err)=>{console.log(err)});
 
-    const app = setupExpress();
+  const app = setupExpress();
 
-    function setupExpress(){
+  function setupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3000, () => {
+      console.log("listening on port 3000");
+    });
 
-        const app = express();
-        const server = http.createServer(app);
-        server.listen(3000, ()=>{
-            console.log('listening on port 3000')
-        });
+    configureExpress(app);
 
-        configureExpress(app);
+    //setup router
+    const router = require("express-promise-router")();
+    users.setRoute(router);
 
-            //setup router
-        const router = require('express-promise-router')();
-        users.setRoute(router);
+    app.use(router);
+  }
 
-        app.use(router);
-    }
+  function configureExpress(app) {
+    app.use(express.static("public"));
+    app.use(cookieParser());
+    app.set("view engine", "ejs");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
+   
+    app.use(
+      session({
+        secret: "mySecretKey",
+        resave: true,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: mongoose.connection}),
+      })
+    );
 
+    app.use(flash());
 
-    function configureExpress(app){
-        app.use(express.static('public'));
-        app.set('view engine', 'ejs');
-        app.use(express.json());
-        app.use(express.urlencoded({extended: true}))
-    }
-
+    app.use(passport.initialize());
+    app.use(passport.session())
+  }
 });
+
