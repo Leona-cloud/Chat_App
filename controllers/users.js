9300bed95b6b1965755a68f8a4cfb145d8c8017c
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-12-14 23:42:32 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,33 @@
 'use strict'
 
-module.exports = function( _ ){
+module.exports = function( _, passport ){
     
     return {
         setRoute: function(router){
-            router.get('/', this.startPage)
+            router.get('/', this.startPage);
+            router.get('/signup', this.getsignUp);
+
+            router.get('/home', this.homePage)
+
+            router.post('/signup', this.postsignUp)
         },
 
         startPage: function(req, res){
             return res.render('start', {start: "This is the start page"})
-        }
+        },
+
+        getsignUp: function(req, res){
+            return res.render('signup')
+        },
+         postsignUp: passport.authenticate('local-signup', {
+             successRedirect: '/home',
+             failureRedirect: '/signup',
+             failureFlash: true
+         }),
+
+         homePage: function(req, res){
+             return res.render('home')
+         }
     }
 
 }
