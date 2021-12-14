67794c54498eb84a6337bf27f2159f4f4b3d6208
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-12-15 00:23:25 +0100

    Add files via upload

---
+++ passport/passport-local.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const passport = require('passport');
+const { User } = require('../models/users');
+const localStrategy = require('passport-local').Strategy;
+
+passport.serializeUser((user, done)=>{
+
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done)=>{
+
+    User.findById(id, (err, user)=>{
+        done(err, user)
+    })
+});
+
+
+
+passport.use('local-signup', new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, (req, email, password, done)=>{
+
+    User.findOne({'email': email}, (err, user)=>{
+        if(err){
+            return done(err)
+        }
+    
+        if(user){
+             return (done, null, req.flash('error', 'user already exists'))
+        };
+
+        let newUser = new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        });
+    
+         newUser.save((err) =>{
+             done(null, newUser)
+         });
+    });
+
+   
+
+  
+    
+
+
+}))
